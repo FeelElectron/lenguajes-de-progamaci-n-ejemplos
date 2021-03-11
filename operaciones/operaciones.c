55f8f5ed46364e12c82f89819077c495e8205e02
Galaxy <47187775+iigalaxy@users.noreply.github.com>
2021-03-11 20:17:58 +0100

    uwuwu

---
+++ operaciones/operaciones.c
@@ -0,0 +1,11 @@
+#include <stidio.h>
+
+int dato1, dato2, resultado;
+dato1 = 10;
+dato2 = 16;
+
+main()
+{
+  resultado = dato1 + dato2; 
+  printf("%d + %d = %d\n", dato1, dato2, resultado)
+} /* Esto sería para hacer una suma pueden cambiarlo por la operación que quieran. */
